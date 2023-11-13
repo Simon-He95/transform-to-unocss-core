d7ddd74f4f149aba200ad51cf1ee160c12413b20
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-11-13 14:36:27 +0800

    fix(toUnocssClass): return type

--- src/toUnocssClass.ts
+++ src/toUnocssClass.ts
@@ -1,12 +1,12 @@
 import { transformStyleToUnocss } from './transformStyleToUnocss'
 
-export function toUnocssClass(css: string, isRem = false) {
+export function toUnocssClass(css: string, isRem = false): [string, string[]] {
   const [transferred, noTransferred] = transformStyleToUnocss(css, isRem)
 
   return [
     transferred
       ? transferred.replace(/="([^"]+)"/g, (_: string, v: string) => `-${v}`)
-      : transferred.replace(/="([^"]+)"/g, (_: string, v: string) => `-${v}`),
+      : '',
     noTransferred,
   ]
 }
