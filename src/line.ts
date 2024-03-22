2544b29cb4bf1a49846756a29ecbbe29f66c7b8d
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-03-22 17:13:06 +0800

    fix(line-height): 1、1.25、1.5、1.625、1.375

--- src/line.ts
+++ src/line.ts
@@ -1,7 +1,15 @@
 import { getVal, transformImportant } from './utils'
 
+const lineMap: Record<string, string> = {
+  1: 'none',
+  1.25: 'tight',
+  1.5: 'normal',
+  1.625: 'relaxed',
+  1.375: 'snug',
+}
 export function line(key: string, val: string) {
   const [value, important] = transformImportant(val)
-
+  if (value in lineMap)
+    return `lh-${lineMap[value]}${important}`
   return `lh${getVal(value)}${important}`
 }
--- test/line.test.ts
+++ test/line.test.ts
@@ -8,8 +8,29 @@ describe('line-height', () => {
   it('px', () => {
     expect(toUnocss('line-height: 20px;')).toBe('lh-20px')
   })
+
   it('em', () => {
     expect(toUnocss('line-height: 20em;')).toBe('lh-20em')
+  })
+
+  it('line-height: 1', () => {
+    expect(toUnocss('line-height: 1;')).toBe('lh-none')
+  })
+
+  it('line-height: 1.25', () => {
+    expect(toUnocss('line-height: 1.25;')).toBe('lh-tight')
+  })
+
+  it('line-height: 1.375', () => {
+    expect(toUnocss('line-height: 1.375;')).toBe('lh-snug')
+  })
+
+  it('line-height: 1.5', () => {
+    expect(toUnocss('line-height: 1.5;')).toBe('lh-normal')
+  })
+
+  it('line-height: 1.625', () => {
+    expect(toUnocss('line-height: 1.625;')).toBe('lh-relaxed')
   })
 
   it('calc', () => {
