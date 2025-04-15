ffe943115b8a89b31accea0fe4fa9593a52a6293
Simon He <57086651+Simon-He95@users.noreply.github.com>
2025-04-15 12:23:48 +0800

    feat: add transformStyleToUnocssPre export to index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { toUnocss } from './toUnocss'
 import { toUnocssClass } from './toUnocssClass'
 import { transformStyleToUnocss } from './transformStyleToUnocss'
+import { transformStyleToUnocssPre } from './transformer'
 
 export * from './utils'
 
-export { toUnocss, toUnocssClass, transformStyleToUnocss }
+export { toUnocss, toUnocssClass, transformStyleToUnocss, transformStyleToUnocssPre }
